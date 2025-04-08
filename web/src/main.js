00c8df3c1eaa32c7200051b90b3f8d32c369aff6
amao <86212616+maomao11223@users.noreply.github.com>
2025-04-08 12:05:34 +0800

    init project

---
+++ web/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './assets/css/style.css'
+import App from './App.vue'
+
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+
+createApp(App).use(Antd).mount('#app')
